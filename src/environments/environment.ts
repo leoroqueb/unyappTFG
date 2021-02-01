ac4840fed541bfce52a6e3750463c6b19dc639f7
Leoroqueb <leoroqueb@gmail.com>
2021-02-01 22:50:38 +0000

    Introduccion de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey:"AIzaSyBomMgwNKz4Bp1JxJAvkIibI4wfI4CueDc",
+    authDomain: "unyapp-leoroqueb.firebaseapp.com",
+    appId: "1:947506461654:web:02fa57ef33da6cc64d4277",
+    projectId: "unyapp-leoroqueb",
+    storageBucket: "unyapp-leoroqueb.appspot.com",
+    messagingSenderId: "947506461654",
+    measurementId: "G-SBGM72GRE4"
+  }
+  
 };
 
 /*
